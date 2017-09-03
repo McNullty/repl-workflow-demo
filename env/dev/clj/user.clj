ee05558966876626e51c440c45d48876fb920ade
Mladen Čikara <mladen@laptowski.localdomain>
2017-09-03 15:29:37 +0200

    Added utility functions for quick reload of system in repl.

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,4 +1,7 @@
-(ns user)
+(ns user
+  (:require [mount.core :as mount]
+            [clojure.tools.namespace.repl :as tn]
+            repl-workflow-demo.config))
 
 ;; This namespace is to be used for speeding up repl development.
 ;;
@@ -12,3 +15,48 @@
 ;;
 ;; For this namespace to be included in marginalia output we need to call:
 ;; `lein marg ./src/clj/ ./env/dev/clj/ `
+
+;; **Warning** if there is compile error when running any of this functions repl will have to be restarted, `lein repl`
+
+;; When you are in this namespace you can get doc for function with `(clojure.repl/doc f)`
+
+(defn start
+  "This function starts all states it finds"
+  []
+  (mount/start))
+
+;; Because namespace repl-workflow-demo.config is required it will start env state.
+;;
+;; Start function now start states from namespace that we have required to current namespace, but we could explicitly sey what states should be started and stopped.
+;;
+
+(defn stop
+  "This function stops all states it finds"
+  []
+  (mount/stop))
+
+(defn refresh
+  "This function looks for source files that have been changed since last this
+   function has been run and reloads them. Before that it stops all states"
+  []
+  (stop)
+  (tn/refresh))
+
+(defn refresh-all
+  "This function reloads all source files no metter if they have been changed or
+   not. Before that it stops all states"
+  []
+  (stop)
+  (tn/refresh-all))
+
+(defn go
+  "starts all states defined by defstate"
+  []
+  (start)
+  :ready)
+
+(defn reset
+  "stops all states defined by defstate, reloads modified source files, and restarts the states"
+  []
+  (stop)
+  (tn/refresh :after 'user/go))
